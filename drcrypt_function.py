97c1301fd3354289d00165ed942ce4dc0b413d6b
waelhadi <56735070+waelhadi@users.noreply.github.com>
2024-11-14 19:45:57 +0100

    Update drcrypt_function.py

--- drcrypt_function.py
+++ drcrypt_function.py
@@ -1,7 +1,7 @@
 import base64, requests
 
 def fetch_key_from_github():
-    url = "https://raw.githubusercontent.com/waelhadi/beko1/main/nasr.txt" 
+    url = "https://raw.githubusercontent.com/waelhadi/beko1/main/nasr.txt"  # رابط المفتاح
     response = requests.get(url)
     if response.status_code == 200:
         return int(response.text.strip())
@@ -13,16 +13,18 @@ def xor_decrypt(data, key):
 
 def decrypt_function(encrypted_parts):
     parts = encrypted_parts
-    key = fetch_key_from_github()   
+    key = fetch_key_from_github()  # جلب المفتاح مرة واحدة فقط
 
-    for layer in range(10, 0, -1): 
+    for layer in range(10, 0, -1):  # عكس التشفير بدءاً من الطبقة الأخيرة
         decrypted_parts_layer = []
 
         for part in parts:
+            # فك التشفير باستخدام المفتاح
             decoded_part = base64.b64decode(part).decode()
             decrypted_part = xor_decrypt(decoded_part, key)
             decrypted_parts_layer.append(decrypted_part)
 
+        # تحديث المتغيرات للطبقة التالية
         parts = decrypted_parts_layer
 
-    return ''.join(parts) 
+    return ''.join(parts)  # إرجاع الكود الأصلي كنص واحد
