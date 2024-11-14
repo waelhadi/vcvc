7aaa56221950fb659d65928d262960381a31d662
waelhadi <56735070+waelhadi@users.noreply.github.com>
2024-11-14 20:21:56 +0100

    Update drcrypt_function.py

--- drcrypt_function.py
+++ drcrypt_function.py
@@ -1,7 +1,7 @@
 import base64, requests
 
 def fetch_key_from_github():
-    url = "https://raw.githubusercontent.com/waelhadi/art1/main/w1213.txt"  # نفس رابط المفتاح المستخدم للتشفير
+    url = "https://raw.githubusercontent.com/waelhadi/art1/main/w1213.txt"
     response = requests.get(url)
     if response.status_code == 200:
         return int(response.text.strip())
@@ -13,13 +13,12 @@ def xor_decrypt(data, key):
 
 def decrypt_function(encrypted_parts):
     parts = encrypted_parts
-    key = fetch_key_from_github()  # جلب المفتاح مرة واحدة
+    key = fetch_key_from_github()
 
-    for layer in range(3, 0, -1):  # عكس التشفير بدءاً من الطبقة الأخيرة
+    for layer in range(3, 0, -1):  # عكس التشفير بدءًا من الطبقة الأخيرة
         decrypted_parts_layer = []
 
         for part in parts:
-            # فك التشفير باستخدام المفتاح
             decoded_part = base64.b64decode(part).decode()
             decrypted_part = xor_decrypt(decoded_part, key)
             decrypted_parts_layer.append(decrypted_part)
